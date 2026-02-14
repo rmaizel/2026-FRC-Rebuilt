b46fbcb7b7b8fe9984478e5cdd51ee7ce88bb79c
Ryan Maizel <rmaizel@team102.org>
2026-02-13 19:03:43 -0500

    Added CommandScheduler from NewCommands VendorDep

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,7 +36,7 @@ public class Robot extends LoggedRobot {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+      CommandScheduler.getInstance().schedule(m_autonomousCommand);
     }
   }
 
